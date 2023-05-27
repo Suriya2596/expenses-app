7e1803fb6ba9930863051dda6af0344cdd5adcd3
Suriya J <94885532+Suriya2596@users.noreply.github.com>
2023-05-27 21:01:50 +0530

    login token

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -20,6 +20,43 @@ userContoller.register = (req,res)=>{
                         })
                 })
         })
+        .catch((err)=>{
+            res.json(err)
+        })
+}
+
+userContoller.login = (req,res)=>{
+    const body = req.body
+    User.findOne({email:body.email})
+        .then((user)=>{
+            if(user){
+                bcryptjs.compare(body.password,user.password)
+                    .then((match)=>{
+                        if(match){
+                            const tokenData = {
+                                _id:user._id,
+                                name: user.name,
+                            } 
+                            const token = jwt.sign(tokenData,process.env.JWT_SECRET,{expiresIn:"2d"})
+                            res.json({token:`Bearer ${token}`})
+                        }else{
+                            res.json({errors: "Invalid email or password",message:"Invalidate email or password"})
+                        }
+                    })
+                    .catch((err)=>{
+                        res.json(err)
+                    })
+            }else{
+                res.json({errors: "Invalid email or password",message:"Invalidate email or password"})
+            }
+        })
+        .catch((err)=>{
+            res.json(err)
+        })
+}
+
+userContoller.account = (res,req)=>{
+    
 }
 
 module.exports = userContoller
--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,11 @@ const routes = express.Router()
 // controller
 const userContoller = require("../app/controllers/userController")
 
+
+// routes
+// user routes
 routes.post("/api/user/register",userContoller.register)
+routes.post("/api/user/login",userContoller.login)
+
 
 module.exports = routes
